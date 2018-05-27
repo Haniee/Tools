6ede3ceb1e72bedf28adbec42df2369c31a29d10
Haniee <michelle@Michelles-MacBook-Air-4.local>
2018-05-27 15:49:27 +1000

    Updated recon file

--- Recon/recon.py
+++ Recon/recon.py
@@ -7,6 +7,7 @@ import threading
 url_list = {}
 host = sys.argv[1]
 brute_list = []
+word_list = []
 
 #bruteforce list 
 
@@ -20,23 +21,50 @@ def bruteforce():
         print("ERROR: cannot open file")
     for line in f.readlines():
         domain = line.rstrip() + "." + host
-        url_status(domain, line)
+        url_status(domain)
         print(domain)
-       
+
+    print(" ***** Finished searching for easy level subdomains *****")
+    try: 
+        f = open('wordlist.txt')
+    except: 
+        print("ERROR: cannot open file")
+
+    for line in f.readlines():
+        for d in brute_list:
+            domain = line.rstrip() + "-" + d.rstrip()
+            url_status(domain)
+            print(domain) 
+    print(" ***** Finished searching for easy level subdomains *****")
 
 
+'''def permutation_words():
+    def second_level():
+    try: 
+        f = open('wordlist.txt')
+    except: 
+        print("ERROR; cannot open file")
+
+
+#create permutations of words 
 def second_level():
-    print("potato")
+    try: 
+        f = open('wordlist.txt')
+    except: 
+        print("ERROR; cannot open file")
+    for line in f.readlines():
+        for d in brute_list:
+            domain = line.rstrip() + "." + d.strip()
+'''
 
-
-def url_status(domain, word):
+def url_status(domain):
     url = "https://" + domain + "/"
     try:
         r = requests.head(url)
-        if(r.status_code == 200 or r.status_code == 401 or r.status_code == 403):
+        if(r.status_code != 404):
             url_list[url] = r.status_code
             update_list(domain)
-            update_wordlist(word)
+            update_wordlist(domain)
     except: 
         print("")
     
@@ -59,7 +87,9 @@ def check_list(domain):
 def update_list(domain):
     brute_list.append(domain)
  	 
-def update_wordlist(word):
+def update_wordlist(domain):
+    #strip word out of domain 
+    word = domain.rstrip(".ns.agency")
     #check if word is already in list 
     if(check_word(word) == True):
         try: 
